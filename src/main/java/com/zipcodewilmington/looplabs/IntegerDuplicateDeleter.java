c1989f686c56c9537800b5988927dd4d2815cdba
Justin <justleland@gmail.com>
2019-07-01 00:13:14 -0400

    finished Lab

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,62 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] array) {
+        super(array);
+    }
+
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] modArray = array;
+
+        for(int i = 0; i < modArray.length; i++) {
+
+            Integer value = modArray[i];
+            int counter = 0;
+
+            for(int j = 0; j < modArray.length; j++) {
+                if(modArray[j].equals(value)) counter++;
+            }
+
+            if(counter >= maxNumberOfDuplications) {
+                modArray = remove(value, modArray, modArray.length - counter);
+                i = -1;
+            }
+        }
+        return modArray;
+    }
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Integer[] modArray = array;
+
+        for(int i = 0; i < modArray.length; i++) {
+
+            Integer value = modArray[i];
+            int counter = 0;
+
+            for(int j = 0; j < modArray.length; j++) {
+                if(modArray[j].equals(value)) counter++;
+            }
+
+            if(counter == exactNumberOfDuplications) {
+                modArray = remove(value, modArray, modArray.length - exactNumberOfDuplications);
+                i = -1;
+            }
+        }
+        return modArray;
+    }
+
+    protected Integer[] remove(Integer value, Integer[] modArray, int arraySize) {
+        Integer[] newArray = new Integer[arraySize];
+        int index = 0;
+
+        for(Integer num : modArray) {
+            if(!num.equals(value)) {
+                newArray[index] = num;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,61 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    public StringDuplicateDeleter(String[] array) {
+        super(array);
+    }
+
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] modArray = array;
+        for(int i = 0; i < modArray.length; i++) {
+
+            String value = modArray[i];
+            int counter = 0;
+
+            for(int j = 0; j < modArray.length; j++) {
+                if(modArray[j].equals(value)) counter++;
+            }
+
+            if(counter >= maxNumberOfDuplications) {
+                modArray = remove(value, modArray, modArray.length - counter);
+                i = -1;
+            }
+        }
+        return modArray;
+    }
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        String[] modArray = array;
+
+        for(int i = 0; i < modArray.length; i++) {
+
+            String value = modArray[i];
+            int counter = 0;
+
+            for(int j = 0; j < modArray.length; j++) {
+                if(modArray[j].equals(value)) counter++;
+            }
+
+            if(counter == exactNumberOfDuplications) {
+                modArray = remove(value, modArray, modArray.length - exactNumberOfDuplications);
+                i = -1;
+            }
+        }
+        return modArray;
+    }
+
+    protected String[] remove(String value, String[] modArray, int arraySize) {
+        String[] newArray = new String[arraySize];
+        int index = 0;
+
+        for(String string : modArray) {
+            if(!string.equals(value)) {
+                newArray[index] = string;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
 }
